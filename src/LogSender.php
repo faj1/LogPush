9bfb513fb0f296b17d92b1434eec8413e3fcd96f
www <222@qq.com>
2024-12-15 01:35:58 +0700

    初始化

--- src/LogSender.php
+++ src/LogSender.php
@@ -2,36 +2,49 @@
 
 namespace Faj1\Push;
 
+use Swoole\Coroutine\Client;
+use function Swoole\Coroutine\run;
+
 class LogSender
 {
 
 
-
     private static string $StaticServerUrl = "";
-    private static string $UdpServerUrl = "";
+    private static array $StaticUdpServerUrl = [];
+private string $serverUrl;
+private array $UdpServer; // 服务器接收日志的接口地址
 
+    /**
+     * 构造函数
+     * @param string $serverUrl 日志接收服务器的 URL（例如：http://example.com/api/logs）
+     */
+    public function __construct(string $serverUrl, array $UdpServer)
+    {
+        $this->serverUrl = $serverUrl;
+        $this->UdpServer = $UdpServer;
+    } // 服务器接收日志的接口地址
 
-    public static  function sendLogData(\Exception $exception, array $context = [], string $level = 'ERROR', string $application = 'default_app', string $environment = 'production'): void
+    public static function sendLogData(\Exception $exception, array $context = [], string $level = 'ERROR', string $application = 'default_app', string $environment = 'production'): void
     {
         $logData = [
-            'log_date'    => date('Y-m-d'),
-            'timestamp'   => date('Y-m-d H:i:s'),
-            'level'       => $level,
-            'message'     => $exception->getMessage(),
+            'log_date' => date('Y-m-d'),
+            'timestamp' => date('Y-m-d H:i:s'),
+            'level' => $level,
+            'message' => $exception->getMessage(),
             'application' => $application,
-            'module'      => null,
-            'logger'      => null,
-            'thread'      => null,
-            'host'        => gethostname(),
-            'user_id'     => $context['user_id'] ?? null,
-            'request_id'  => $context['request_id'] ?? null,
-            'context'     => json_encode($context),
-            'exception'   => $exception->getTraceAsString(),
+            'module' => null,
+            'logger' => null,
+            'thread' => null,
+            'host' => gethostname(),
+            'user_id' => $context['user_id'] ?? null,
+            'request_id' => $context['request_id'] ?? null,
+            'context' => json_encode($context),
+            'exception' => $exception->getTraceAsString(),
             'environment' => $environment,
-            'file_name'   => $exception->getFile(),
+            'file_name' => $exception->getFile(),
             'line_number' => $exception->getLine(),
         ];
-        if(!self::$StaticServerUrl or !self::$UdpServerUrl){
+        if (!self::$StaticServerUrl or !self::$StaticUdpServerUrl) {
             // 获取用户项目的根目录路径
             $projectRoot = dirname(__DIR__, 4); // 根据实际路径调整，假设当前库代码在项目的 vendor/your-vendor/your-package/ 目录下
             $configFile = $projectRoot . '/LogConfig.json';
@@ -40,51 +53,31 @@ class LogSender
                 if (json_last_error() !== JSON_ERROR_NONE) {
                     throw new \RuntimeException('配置文件 config.json 格式错误: ' . json_last_error_msg());
                 }
-                if(!isset($config['ServerUrl'])){
+                if (!isset($config['ServerUrl'])) {
                     throw new \RuntimeException('缺少必备的配置项:ServerUrl');
-                }else{
+                } else {
                     self::$StaticServerUrl = $config['ServerUrl'];
                 }
-                if(!isset($config['UdpServerUrl'])){
-                    throw new \RuntimeException('缺少必备的配置项:UdpServerUrl');
-                }else{
-                    self::$UdpServerUrl = $config['UdpServerUrl'];
+                if (!isset($config['UdpServerHost']) or !isset($config['Port'])) {
+                    throw new \RuntimeException('缺少必备的配置项:UdpServerHost或Port');
+                } else {
+                    self::$StaticUdpServerUrl['host'] = $config['UdpServerHost'];
+                    self::$StaticUdpServerUrl['Port'] = $config['Port'];
                 }
-
-                // 使用配置数据
-                var_dump($config);
             } else {
-                throw new \RuntimeException('配置文件 config.json 不存在,获取到的根目录路径为:'.$projectRoot);
+                throw new \RuntimeException('配置文件 config.json 不存在,获取到的根目录路径为:' . $projectRoot);
             }
         }
 
+        if (extension_loaded('swoole')) {
+            $LogSender = new LogSender(self::$StaticServerUrl, self::$StaticUdpServerUrl);
+            $LogSender->sendLog($logData);
+        } else {
+            $LogSender = new LogSender(self::$StaticServerUrl, self::$StaticUdpServerUrl);
+            $LogSender->sendLog($logData);
+        }
 
 
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-    private string $serverUrl; // 服务器接收日志的接口地址
-
-    /**
-     * 构造函数
-     * @param string $serverUrl 日志接收服务器的 URL（例如：http://example.com/api/logs）
-     */
-    public function __construct(string $serverUrl)
-    {
-        $this->serverUrl = $serverUrl;
     }
 
     /**
@@ -127,6 +120,24 @@ class LogSender
         curl_exec($ch);
         // 忽略请求结果和可能出现的错误，确保不抛出异常
         curl_close($ch);
+    }
+
+    public function UdpSendLog(array $logData): void
+    {
+        try {
+            run(function () use ($logData) {
+                $client = new Client(SWOOLE_SOCK_UDP);
+                if (!$client->connect($this->UdpServer['UdpServerHost'], $this->UdpServer['Port'], 0.5)) {
+                    echo "connect failed. Error: {$client->errCode}\n";
+                }
+                $client->send(json_encode($logData));
+                echo $client->recv();
+                $client->close();
+            });
+        } catch (\Exception $e) {
+            // 捕捉任何异常，不做任何错误处理，保证系统不中断
+        }
+
     }
 
 }
