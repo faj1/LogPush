c48e8972134be00888121f9850bc6eca5fcd1945
www <222@qq.com>
2024-12-15 02:06:18 +0700

    初始化

--- src/LogSender.php
+++ src/LogSender.php
@@ -3,10 +3,7 @@
 namespace Faj1\Push;
 
 use Exception;
-use JsonException;
-use RuntimeException;
-use Swoole\Coroutine\Client;
-use function Swoole\Coroutine\run;
+
 
 class LogSender
 {
@@ -22,7 +19,7 @@ class LogSender
      * @param string $level 日志级别
      * @param string $application 应用名称
      * @param string $environment 运行环境
-     * @throws RuntimeException|JsonException
+     * @throws Exception
      */
     public static function sendLogData(
         Exception $exception,
@@ -33,42 +30,47 @@ class LogSender
     ): void {
         self::initializeConfig();
 
-        $logData = [
-            'log_date'    => date('Y-m-d'),
-            'timestamp'   => date('Y-m-d H:i:s'),
-            'level'       => $level,
-            'message'     => $exception->getMessage(),
-            'application' => $application,
-            'module'      => null,
-            'logger'      => null,
-            'thread'      => null,
-            'host'        => gethostname(),
-            'user_id'     => $context['user_id'] ?? null,
-            'request_id'  => $context['request_id'] ?? null,
-            'context'     => json_encode($context, JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR),
-            'exception'   => $exception->getTraceAsString(),
-            'environment' => $environment,
-            'file_name'   => $exception->getFile(),
-            'line_number' => $exception->getLine(),
-        ];
+        try {
+            $logData = [
+                'log_date' => date('Y-m-d'),
+                'timestamp' => date('Y-m-d H:i:s'),
+                'level' => $level,
+                'message' => $exception->getMessage(),
+                'application' => $application,
+                'module' => null,
+                'logger' => null,
+                'thread' => null,
+                'host' => gethostname(),
+                'user_id' => $context['user_id'] ?? null,
+                'request_id' => $context['request_id'] ?? null,
+                'context' => json_encode($context, JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR),
+                'exception' => $exception->getTraceAsString(),
+                'environment' => $environment,
+                'file_name' => $exception->getFile(),
+                'line_number' => $exception->getLine(),
+            ];
+            $logPush = new LogPush(self::$staticServerUrl, self::$staticUdpServerUrl, self::$debug);
+            if(self::$debug){
+                echo '配置的服务器链接为:'.self::$staticServerUrl.PHP_EOL;
+                echo '配置的UDP服务器信息为:'.json_encode(self::$staticServerUrl).PHP_EOL;
+            }
 
-        $logPush = new LogPush(self::$staticServerUrl, self::$staticUdpServerUrl, self::$debug);
-        if(self::$debug){
-            echo '配置的服务器链接为:'.self::$staticServerUrl.PHP_EOL;
-            echo '配置的UDP服务器信息为:'.json_encode(self::$staticServerUrl).PHP_EOL;
-        }
-
-        if (extension_loaded('swoole')) {
-            $logPush->UdpSendLog($logData);
-        } else {
-            $logPush->sendLog($logData);
+            if (extension_loaded('swoole')) {
+                $logPush->UdpSendLog($logData);
+            } else {
+                $logPush->sendLog($logData);
+            }
+        } catch (\Throwable $e) {
+            if(self::$debug){
+                echo '出错了:'.$e->getMessage().PHP_EOL;
+            }
         }
     }
 
     /**
      * 初始化配置文件
      *
-     * @throws RuntimeException
+     * @throws Exception
      */
     private static function initializeConfig(): void
     {
@@ -80,24 +82,24 @@ class LogSender
             $projectRoot = dirname(__DIR__, 4); // 假设当前库代码在 vendor/your-vendor/your-package/
             $configFile = $projectRoot . '/LogConfig.json';
             if (!file_exists($configFile)) {
-                throw new RuntimeException('配置文件 LogConfig.json 不存在, 目录路径为: ' . $projectRoot);
+                throw new Exception('配置文件 LogConfig.json 不存在, 目录路径为: ' . $projectRoot);
             }
             $config = json_decode(file_get_contents($configFile), true, 512, JSON_THROW_ON_ERROR);
             if (!is_array($config)) {
-                throw new RuntimeException('配置文件 LogConfig.json 格式错误，解析结果非数组');
+                throw new Exception('配置文件 LogConfig.json 格式错误，解析结果非数组');
             }
-            self::$staticServerUrl = $config['ServerUrl'] ?? throw new RuntimeException('缺少必备的配置项: ServerUrl');
+            self::$staticServerUrl = $config['ServerUrl'] ?? throw new Exception('缺少必备的配置项: ServerUrl');
 
             if (!isset($config['UdpServerHost']) || !isset($config['Port'])) {
-                throw new RuntimeException('缺少必备的配置项: UdpServerHost 或 Port');
+                throw new Exception('缺少必备的配置项: UdpServerHost 或 Port');
             }
             self::$staticUdpServerUrl = [
                 'host' => $config['UdpServerHost'],
                 'port' => $config['Port'],
             ];
             self::$debug = $config['debug'] ?? false; // 默认值为 false
-        } catch (JsonException $e) {
-            throw new RuntimeException('JsonException: ' . $e->getMessage());
+        } catch (\Throwable $e) {
+            throw new Exception('JsonException: ' . $e->getMessage());
         }
     }
 }
